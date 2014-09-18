247174bfc49c1e3aa95c51e01e821ce677760fba
Anthony Anderson <LordIllyohs@gmail.com>
2014-09-18 02:57:05 -0400

    MOAR SHAPE FUNCTIONS
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- common/us/illyohs/azathoth/shapes/UtilPyramidFuntions.java
+++ common/us/illyohs/azathoth/shapes/UtilPyramidFuntions.java
@@ -28,38 +28,15 @@ package us.illyohs.azathoth.shapes;
 import java.util.HashSet;
 
 import us.illyohs.azathoth.world.WorldXYZ;
-import net.minecraft.world.World;
 
-public class UtilSphericalFunctions {
-
-    public static boolean radiusCheck(int x, int y, int z, int rd) {
-        return ((x * x) + (y * y) + (z * z) < ((rd + 0.5) * (rd + 0.5)));
+public class UtilPyramidFuntions {
+    
+    public static HashSet<WorldXYZ> getPyramid(WorldXYZ coords, int length, int width, int hight) {
+        //TODO: Work out the logic for Pyramids 
+            //For this sudo code length = L, width = W, hight = H
+                //So LxW for the base then layers L-1xW-1 for each layer and center till it reaches the top
+        return null;
+        
     }
     
-    public static HashSet<WorldXYZ> getSphere (WorldXYZ coords, int radius) {
-        float r_squared = (float)((radius + 0.5) * (radius + 0.5));
-        World world = coords.getWorld();
-        HashSet<WorldXYZ> returnvalues = new HashSet<WorldXYZ>();
-        //loop needs to cap at the top and bottom of the world
-        int bottom = Math.max(-radius - 1,  -1*(coords.posY - 1));
-        int top = Math.min(radius + 1, (255 - coords.posY));//get from the server.properties
-        for (int y = bottom; y < top; y++)  {
-            for (int z = -radius-1; z < radius+1; z++) {
-                for (int x = -radius-1; x < radius+1; x++) {
-                    if((x * x) + (y * y) + (z * z) < r_squared) {
-                        returnvalues.add(new WorldXYZ(world, coords.posX + x, coords.posY + y, coords.posZ + z));
-                    }
-                }
-            }
-        }
-        return returnvalues;
-    }
-	
-	public static HashSet<WorldXYZ> getShell(WorldXYZ center, int radius){
-	    //Josiah: I wrote this so it's probably got holes...
-	    HashSet<WorldXYZ> bigSphere = getSphere(center, radius);
-	    HashSet<WorldXYZ> smallerSphere = getSphere(center, radius-1);
-        bigSphere.removeAll(smallerSphere);
-	    return bigSphere;
-	}
 }
--- common/us/illyohs/azathoth/shapes/UtilRectangularFuntions.java
+++ common/us/illyohs/azathoth/shapes/UtilRectangularFuntions.java
@@ -28,38 +28,14 @@ package us.illyohs.azathoth.shapes;
 import java.util.HashSet;
 
 import us.illyohs.azathoth.world.WorldXYZ;
-import net.minecraft.world.World;
 
-public class UtilSphericalFunctions {
-
-    public static boolean radiusCheck(int x, int y, int z, int rd) {
-        return ((x * x) + (y * y) + (z * z) < ((rd + 0.5) * (rd + 0.5)));
-    }
+public class UtilRectangularFuntions {
     
-    public static HashSet<WorldXYZ> getSphere (WorldXYZ coords, int radius) {
-        float r_squared = (float)((radius + 0.5) * (radius + 0.5));
-        World world = coords.getWorld();
-        HashSet<WorldXYZ> returnvalues = new HashSet<WorldXYZ>();
-        //loop needs to cap at the top and bottom of the world
-        int bottom = Math.max(-radius - 1,  -1*(coords.posY - 1));
-        int top = Math.min(radius + 1, (255 - coords.posY));//get from the server.properties
-        for (int y = bottom; y < top; y++)  {
-            for (int z = -radius-1; z < radius+1; z++) {
-                for (int x = -radius-1; x < radius+1; x++) {
-                    if((x * x) + (y * y) + (z * z) < r_squared) {
-                        returnvalues.add(new WorldXYZ(world, coords.posX + x, coords.posY + y, coords.posZ + z));
-                    }
-                }
-            }
-        }
-        return returnvalues;
+    public static HashSet<WorldXYZ> getRectangularPrism(WorldXYZ coords, int length, int width, int hight) {
+        //TODO: Work out the logic for rectangular prisms
+            //For this sudo code length = L, width = W, hight = H
+                //So LxWxH
+        return null;
+        
     }
-	
-	public static HashSet<WorldXYZ> getShell(WorldXYZ center, int radius){
-	    //Josiah: I wrote this so it's probably got holes...
-	    HashSet<WorldXYZ> bigSphere = getSphere(center, radius);
-	    HashSet<WorldXYZ> smallerSphere = getSphere(center, radius-1);
-        bigSphere.removeAll(smallerSphere);
-	    return bigSphere;
-	}
 }
