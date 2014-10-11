92c8130077b282527122163782980bbee7d9cb77
Anthony Anderson <LordIllyohs@gmail.com>
2014-10-10 22:56:31 -0400

    Moved more internal stuff into the core pachage and added pulsar.
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- common/us/illyohs/azathoth/core/irc/Base64.java
+++ common/us/illyohs/azathoth/core/irc/Base64.java
@@ -23,7 +23,7 @@
  *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package us.illyohs.azathoth.irc;
+package us.illyohs.azathoth.core.irc;
 
 public class Base64 {
 }
--- common/us/illyohs/azathoth/core/irc/irclib.java
+++ common/us/illyohs/azathoth/core/irc/irclib.java
@@ -24,7 +24,7 @@
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package us.illyohs.azathoth.irc;
+package us.illyohs.azathoth.core.irc;
 
 public class irclib {
 }
