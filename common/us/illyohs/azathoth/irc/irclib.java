9017652ed941c4d1036202c6e483c86e18acedc3
Anthony Anderson <LordIllyohs@gmail.com>
2014-10-10 21:35:30 -0400

    Removed everything related to energy and start work on ircstuffs
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- common/us/illyohs/azathoth/irc/irclib.java
+++ common/us/illyohs/azathoth/irc/irclib.java
@@ -1,17 +1,17 @@
 /**
  *  Copyright (c) 2014, Runix-Minecraft
  *  All rights reserved.
- *  
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
- *  
+ *
  *  * Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- *    
+ *
  *  * Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *  
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,40 +23,8 @@
  *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package us.illyohs.azathoth.energy;
 
-public interface IBlockEnergy {
-    
-    /*
-     * Gets the current amount of energy an item has.
-     */
-    public int getCurrentEnergy(int current);
+package us.illyohs.azathoth.irc;
 
-    /*
-     * Sets the maximum energy an item can hold.
-     */
-    public int getMaxEnrgy(int max);
-
-    /*
-     * Sets the energy to the block holding the energy.
-     */
-    public int setEnergy(int set);
-    
-    /*
-     * Sets the amount of energy that the block can take in.
-     */
-    public int setInput(int input);
-    
-    /*
-     * Sets the amount of energy a block can output.
-     */
-    public int setOutput(int output);
-    
-    /*
-     * Determines whether the tileentity can be broken without dropping its
-     * energy.
-     */
-    public boolean isHarvestable();
-
-
+public class irclib {
 }
