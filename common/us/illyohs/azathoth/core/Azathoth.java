96f92e605c45a92302cafb40106b17ea28843d71
Anthony Anderson <LordIllyohs@gmail.com>
2014-10-11 00:00:00 -0400

    Started work on a command base

--- common/us/illyohs/azathoth/core/Azathoth.java
+++ common/us/illyohs/azathoth/core/Azathoth.java
@@ -26,8 +26,28 @@
 package us.illyohs.azathoth.core;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
 public class Azathoth {
+	
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
+		
+	}
 
 }
